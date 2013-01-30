53f83f43ba472203205ccc57f21a83a3c5ac2aca
George Sakkis <george@podio.com>
2013-01-30 13:39:52 +0100

    Added setup.py, closes #5

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="pypodio2",
+    version="0.1",
+    description="Python wrapper for the Podio API",
+    author="Podio",
+    author_email="mail@podio.com",
+    url="https://github.com/podio/podio-py",
+    license="MIT",
+    packages=["pypodio2"],
+    install_requires=["httplib2"],
+    tests_require=["nose", "mock"],
+    test_suite="nose.collector",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
